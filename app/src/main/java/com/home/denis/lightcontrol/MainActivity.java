c5e3d92719eea55f97980c0494d6c772a6b35c50
denisys <denisys@nomail.com>
2017-09-29 18:03:21 +0300

    Изменена иконка программы

--- app/src/main/java/com/home/denis/lightcontrol/MainActivity.java
+++ app/src/main/java/com/home/denis/lightcontrol/MainActivity.java
@@ -1,8 +1,11 @@
 package com.home.denis.lightcontrol;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(item.getTitle());
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                break;
+            case R.id.menu_exit:
+                finish();
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 }
